2af286efd26bee6ea5ea71e19c5b2514eeee6f10
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-28 21:35:45 +0530

    Update grade.model.js

--- models/grade.model.js
+++ models/grade.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const gradeSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
-  grade: { type: Number },
   assignment: { type: mongoose.Schema.Types.ObjectId, ref: "Assignment" },
   quiz: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
-  submissionDate: { type: Date, default: Date.now },
+  grade: { type: Number },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Grade = mongoose.model("Grade", gradeSchema);
