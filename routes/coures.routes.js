c27fa223007611c84267ceabaa1c482bd2934277
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-28 21:36:12 +0530

    Update coures.routes.js

--- routes/coures.routes.js
+++ routes/coures.routes.js
@@ -1,15 +1,30 @@
 const { Router } = require("express");
-const { addCoures, updateCourse, deleteCourse, selfEnroll, submitAssignment, enrollStudent, removeStudent, getEnrolledCourses, getMySubmissions, updateSubmission, deleteSubmission } = require("../controllers/coures.controller")
+const {
+  addCoures,
+  updateCourse,
+  deleteCourse,
+  selfEnroll,
+  submitAssignment,
+  enrollStudent,
+  removeStudent,
+  getEnrolledCourses,
+  getMySubmissions,
+  updateSubmission,
+  deleteSubmission,
+} = require("../controllers/coures.controller");
 const { Auth, authorize, admin, student } = require("../middlewares/auth");
 const courseRoute = Router();
 
-courseRoute.post("/add", authorize, addCoures);
-courseRoute.patch("/upadate/:id", Auth, authorize, updateCourse);
+courseRoute.post("/add", admin, addCoures);
+courseRoute.patch("/upadate/:id", admin, updateCourse);
 courseRoute.delete("/delete/:id", admin, deleteCourse);
+
 courseRoute.post("/self", Auth, student, selfEnroll);
 courseRoute.post("/submit", Auth, student, submitAssignment);
+
 courseRoute.post("/enroll", Auth, admin, enrollStudent);
 courseRoute.post("/remove", Auth, admin, removeStudent);
+
 courseRoute.get("/getenroll", Auth, student, getEnrolledCourses);
 courseRoute.get("/my-submissions", Auth, student, getMySubmissions);
 courseRoute.patch("/upadatesub/:id", Auth, student, updateSubmission);
